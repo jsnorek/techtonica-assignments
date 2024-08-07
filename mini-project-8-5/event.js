40739b66177dd9fd25941cf90bc3fbf3d7f2fab3
Juliana <jsnorek340@gmail.com>
2024-08-07 08:29:49 -0700

    extra comment

--- mini-project-8-5/event.js
+++ mini-project-8-5/event.js
@@ -1,27 +1,48 @@
+class TicketType {
+  constructor (ticketName, price) {
+    this.ticketName = ticketName;
+    this.price = price;
+  }
+}
+
 class Event {
     constructor(name, description) {
       this.name = name;
       this.description = description;
       this.availableTickets = [];
-    }
-      addAvailableTickets(ticketName, ticketPrice) {
-        const ticket = new TicketType(ticketName, ticketPrice);
-        this.availableTickets.push(ticket);
       }
-      /*
+      addAvailableTickets(ticketName, price) {
+        const newTicket = new TicketType(ticketName, price)
+        this.availableTickets.push(newTicket);
+      }
       allTickets() {
-        return "All tickets: " + this.availableTickets.map(tickets, index) => `${index + 1}`
-      } */
-  }
+        return "All tickets: " + this.availableTickets.map((ticket, index) => `${index + 1}. ${ticket.name} ($${ticket.price})`).join(" ")
+      }
+    }
+  //extra comment
   
   // The below statement creates an object.
   const eventObj1 = new Event(
     'KLOS Golden Gala',
     'An evening with hollywood vampires'
   );
+  eventObj1.addAvailableTickets("human", 299);
+  eventObj1.addAvailableTickets("vampire", 99);
+
   
-  const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+  const eventObj2 = new Event(
+    'Skillet & Sevendust', 
+    'Victorious war tour'
+  );
+  eventObj2.addAvailableTickets("General Admission", 25);
+  eventObj2.addAvailableTickets("Floor Seating ", 80);
+
   const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+  eventObj3.addAvailableTickets("Orchestra", 300);
+  eventObj3.addAvailableTickets("Mezzanine", 200);
+  eventObj3.addAvailableTickets("Balcony", 100);
+
+  console.log(eventObj1.availableTickets);
   
   const eventArray = new Array();
   
@@ -41,10 +62,7 @@ class Event {
     });
     document.querySelector('#event').innerHTML = html;
   });
+
+
   
-  class TicketType {
-    constructor (ticketName, ticketDescription) {
-      this.ticketName = ticketName;
-      this.ticketDescription = ticketDescription;
-    }
-  }
+

afc004dc5e3dce52bbb12f2329fefd14710eb150
Juliana <jsnorek340@gmail.com>
2024-08-08 20:04:31 -0700

    added final js steps

--- mini-project-8-5/event.js
+++ mini-project-8-5/event.js
@@ -18,6 +18,14 @@ class Event {
       allTickets() {
         return "All tickets: " + this.availableTickets.map((ticket, index) => `${index + 1}. ${ticket.name} ($${ticket.price})`).join(" ")
       }
+
+      searchTickets(lowerPrice, upperPrice) {
+        const eligibleTickets = this.availableTickets.filter(ticket => ticket.price >= lowerPrice && ticket.price <= upperPrice);
+        if(eligibleTickets.length === 0) {
+          return "No tickets available";
+        }
+        return "Eligible tickets: " + eligibleTickets.map((ticket, index) => `${index + 1}. ${ticket.ticketName} ($${ticket.price})`).join(" ");
+      }
     }
 
   
@@ -57,12 +65,20 @@ class Event {
   document.addEventListener('DOMContentLoaded', () => {
     // Handler when the DOM is fully loaded
     let html = '';
-    eventArray.forEach((item) => {
-      html += `<li>${item.name} - ${item.description}`;
+    eventArray.forEach((event) => {
+      html += `<li>${event.name} - ${event.description} - ${event.allTickets()}</li>)`;
     });
     document.querySelector('#event').innerHTML = html;
   });
 
+  document.addEventListener('DOMContentLoaded', () => {
+    let html = '';
+    eventArray.forEach((event) => {
+      html += `<li> ${event.name} - ${event.description} - ${event.searchTickets(0, 100)}</li>`
+    })
+    document.querySelector('#event').innerHTML = html;
+}) 
 
+console.log("Test case 1:", eventObj3.searchTickets(0, 250)); 
   
 

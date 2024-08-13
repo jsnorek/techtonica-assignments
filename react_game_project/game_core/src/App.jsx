672a8efcd95553b5739431ea96634a2b27533139
Juliana <jsnorek340@gmail.com>
2024-08-13 12:22:29 -0700

    beginning steps for game project. created ship and started building the asteroid. ship can now move left and right from arrow keys. asteroid drops down screen in timed incremenets

---
+++ react_game_project/game_core/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+
+function App() {
+  const [position, setPosition] = useState(0);
+  const [asteroid, setAsteroid] = useState({ x: 0, y: 100 });
+
+  function handleKeyDown(e) {
+    if (e.key === "ArrowLeft") {
+      if (position >= 0) {
+        setPosition((prev) => prev - 10);
+      }
+    } else if (e.key === "ArrowRight") {
+      if (position <= 500) {
+      setPosition((prev) => prev + 10);
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAsteroid((prev) => {
+        return {
+          ...prev,
+          y: prev.y + 10
+        }
+      })
+    }, 1000)
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="ship" style={{ transform: `translateX(${position}px)` }}>
+        ship
+      </div>
+      <div className="asteriods" style={{ top: `${asteroid.y}px`, left: `${asteroid.x}px` }}>
+        asteroid
+      </div>
+    </div>
+  );
+}
+
+export default App;

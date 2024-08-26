08ba72656b1adaafde393a8d0276d1b14bce7f97
Juliana <jsnorek340@gmail.com>
2024-08-26 11:38:04 -0700

    start project, starting components and adding font awesome fonts + images for weather

---
+++ weather-app/src/Components/WeatherApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherApp = () => {
+  return (
+    <div className='container'>
+        <div className='weather-app'></div>
+        <div className="search"></div>
+        <div className="search-top"></div>
+    </div>
+  )
+}
+
+export default WeatherApp

caf11fc1dbbe1f48904894bcab97a0d2f804e06d
Juliana <jsnorek340@gmail.com>
2024-09-02 15:41:50 -0700

    added user input and answer count

--- full-stack-trivia/client/src/App.jsx
+++ full-stack-trivia/client/src/App.jsx
@@ -1,45 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [questionData, setQuestionData] = useState(null);
-
-  // useEffect(() => {
-  //   fetch('/api')
-  //     .then(response => response.json())
-  //     .then(data => setData(data.message));
-  // }, []);
-
-
-
-  // useEffect(() => {
-    
-  //     console.log('useEffect is triggered')
-  //     fetch('http://localhost:5005/', {
-  //       headers: {
-  //         "Content-Type": "application/json"
-  //       }
-  //     })
-  //       .then((res) => {
-  //         console.log(res);
-  //         return res.json()})
-  //       .then((data) => {
-  //         console.log(data); 
-  //         setQuestionData(data)})
-  //       .catch((err) => console.log(err));
-    
-  // }, []);
+  const [question, setQuestion] = useState(null);
+  const [answer, setAnswer] = useState("");
+  const [answerCount, setAnswerCount] = useState(0);
+  const [wrongAnswer, setWrongAnswer] = useState(0);
+  const [rightAnswer, setRightAnswer] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('/api/questions', {
+        const res = await fetch("/api/questions", {
           headers: {
-            "Content-Type": "application/json"
-          }
+            "Content-Type": "application/json",
+          },
         });
-        const {results} = await res.json();
+        const { results } = await res.json();
         setQuestionData(results);
+        setQuestion(results[0]);
       } catch (err) {
         console.error(err);
       }
@@ -48,27 +29,58 @@ function App() {
     fetchData();
   }, []);
 
-  
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-//   return (
-//     <div>
-//       {/* <h1>{data ? data : 'Loading...'}</h1> */}
-//       <p>Questions</p>
-//       {data ? (
-//         <>
-//           <p>{data.results}</p>
-//         </>
-//       ) : (
-//         <p>Loading data...</p>
-//       )}
-//     </div>
-//   );
-// }
-console.log("test", questionData);
-   return (
-      <div>
-        <p>Questions</p>
-        {questionData ? (
+    if (gameOver) {
+      return;
+    }
+
+    setAnswerCount((prevCount) => {
+      return prevCount + 1
+    })
+    //change user input and database "correct answer" to lowercase to they're always matching despite any capital or lowercase letters
+    const correctAnswer = question.correct_answer.toLowerCase();
+    const userAnswer = answer.toLowerCase();
+
+    if (userAnswer === correctAnswer) {
+      setRightAnswer((prevCount) => {
+        return prevCount + 1
+      })
+    } else {
+      setWrongAnswer((prevCount) => {
+        return prevCount + 1
+      })
+    }
+    
+    console.log(answerCount, "answer count")
+
+    if (questionData.length > answerCount + 1) {
+      const newQuestion = questionData[answerCount + 1];
+      setQuestion(newQuestion);
+    } else {
+      setGameOver(true);
+      window.alert("game over")
+    }
+  };
+
+  // console.log("test", questionData);
+  // console.log(answerCount, "count")
+  console.log("right", rightAnswer, "wrong", wrongAnswer);
+  return (
+    <div>
+      <h1>Questions</h1>
+      {question && <h3> {question.question}</h3>}
+      <form onSubmit={onSubmit}>
+        <input
+          name="answer"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* {questionData ? (
           questionData.map((question, index) => (
             <div key={index}>
               <p>Category: {question.category}</p>
@@ -79,10 +91,9 @@ console.log("test", questionData);
           ))
         ) : (
           <p>Loading data...</p>
-        )}
-      </div>
-    );
-
+        )} */}
+    </div>
+  );
 }
 
 export default App;

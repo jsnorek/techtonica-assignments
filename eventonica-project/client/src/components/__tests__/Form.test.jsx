dddf71970fb866bc4507c41913021aa0d5b93ffc
Juliana <jsnorek340@gmail.com>
2024-09-12 18:52:21 -0700

    added two working tests for the form

--- eventonica-project/client/src/components/__tests__/Form.test.jsx
+++ eventonica-project/client/src/components/__tests__/Form.test.jsx
@@ -1,5 +1,6 @@
 import { test, expect, vi } from "vitest";
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 import Form from  "../Form";
 
 test('renders Form component correctly', () => {
@@ -8,4 +9,19 @@ test('renders Form component correctly', () => {
     expect(getByPlaceholderText("Location")).toBeDefined();
 
     expect(getByText("Location")).toBeDefined();
+});
+
+test('Form renders our mock data correctly', () => {
+    render(<Form onSaveEvent={vi.fn()} onUpdateEvent={vi.fn()} />);
+    const titleInput = screen.getByPlaceholderText('Title');
+  const locationInput = screen.getByPlaceholderText('Location');
+//   const dateInput = screen.getByText('Date');
+
+  fireEvent.change(titleInput, { target: { value: 'New Event' } });
+  fireEvent.change(locationInput, { target: { value: 'New Location' } });
+//   fireEvent.change(dateInput, { target: { value: '2024-09-12' } });
+
+  expect(titleInput.value).toBe('New Event');
+  expect(locationInput.value).toBe('New Location');
+//   expect(dateInput.value).toBe('2024-09-12');
 });

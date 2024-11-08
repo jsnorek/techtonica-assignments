47e098ca89559535569bd51ea9f738ca5bb7b289
Juliana <jsnorek340@gmail.com>
2024-11-08 15:30:18 -0800

    cleaned up asteroid file

--- react_game_project/game_core/src/components/Asteroid.jsx
+++ react_game_project/game_core/src/components/Asteroid.jsx
@@ -1,59 +1,58 @@
+// Handles the asteroid and ship rendering and movement on the screen
 import { useEffect, useState } from "react";
 
-//position here refers to the position of the ship. both position and setScore are pulled from App
+// Position here refers to the position of the ship. both position and setScore are pulled from App
 function Asteroid({position, setScore}) {
-  //console.log("tester");
 
-  //asteroid holds the "set" position of the asteroid
-  //useState is creating the default position of asteroid
+
+  // Creating the default position of asteroid
   const [asteroid, setAsteroid] = useState({ x: generator(500), y: generator(700)});
+  // Handles end of game
   const [gameOver, setGameOver] = useState(0);
-  //create a random number using max boundary as a way to randomly position asteroids
+  // Create a random number using max boundary as a way to randomly position asteroids
   function generator(maxBoundary) {
     const randomNum = Math.floor(Math.random() * (maxBoundary + 1));
     return randomNum;
   }
   
-  //sets the timed interval for asteroids to drop down the screen
   
-  //this useEffect is checking if the asteroid is hitting the ship position
+  // Checking if the asteroid is hitting the ship position
   useEffect(() => {
-    //end of the browser window
+    // End of the browser window
     const shipY = window.innerHeight - 50;
-    //checking if asteroid reaches bottom of browser window, the rest is checking the position of the asteroid and if they've collided
+    // Checking if asteroid reaches bottom of browser window, the rest is checking the position of the asteroid and if they've collided
     if(asteroid.y >= shipY && (asteroid.x <= position + 50 && asteroid.x >= position - 50)) {
       console.log("game over");
-      //if asteroid hits ship from line 24, then gameOver state equals 1 and setScore changes to add 100 to score
+      // If asteroid hits ship from line 24, then gameOver state equals 1 and setScore changes to add 100 to score
       if(gameOver === 0) {
       setScore((prev) => {
         return prev += 100;
       });}
     }
-    //useState variable/dependencies are added to the [] for recall ability
   }, [asteroid, position, setScore])
   
-  //this useEffect moves the asteroid and stops movement if it hits the bottom of screen
+  // This useEffect moves the asteroid and stops movement if it hits the bottom of screen
   useEffect(() => {
   const interval = setInterval(() => {
     console.log(asteroid.y);
     console.log(window.innerHeight);
-   //checks if gameOver is still 0 after each asteroid movement, holds the same score
+   // Checks if gameOver is still 0 after each asteroid movement, holds the same score
     if (gameOver === 0) {
     setAsteroid((prev) => {
       
-      //this stops interval when asteroid hits the bottom of the window
+      // This stops interval when asteroid hits the bottom of the window
       if(prev.y >= window.innerHeight - 50) {
         console.log("passed window");
         clearInterval(interval);
-        //sets gameover to 1 so it stops all above functions that rely on gameOver === 0, esentially stopping the game
+        // Sets gameover to 1 so it stops all above functions that rely on gameOver === 0, esentially stopping the game
         setGameOver(1);
-        //stops asteroid movement
+        // Stops asteroid movement
         return {
           ...prev,
           y: prev.y
         }
       }
-      //moves asteroid down the y axis in increments of 10  
+      // Moves asteroid down the y axis in increments of 10  
       return {
         ...prev,
         y: prev.y + 20
@@ -61,7 +60,7 @@ function Asteroid({position, setScore}) {
     }) }
   }, 1000)
   }, [])
-  //setting asteroid to random position with style 
+  // Setting asteroid to a random position using inline styling
   return (<div className="asteriods" style={{ top: `${asteroid.y}px`, left: `${asteroid.x}px` }}>
     asteroid
   </div> );
